2251d80b25a9c3184593220a24a3a9c5c4c26ec7
Walid Arghandewal <walid.argh@gmail.com>
2016-04-27 06:56:13 -0700

    router config

--- frontend/owl.jsx
+++ frontend/owl.jsx
@@ -1,8 +1,31 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var LoginForm = require('./components/LoginForm');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+
+var App = React.createClass({
+  render: function(){
+    return (
+        <div>
+          <header><h1>Bench BnB</h1></header>
+          {this.props.children}
+        </div>
+    );
+  }
+});
+
+var Router = {
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
+			<Route path="user/"
+		</Route>
+	</Router>
+}
 
 document.addEventListener("DOMContentLoaded", function () {
 	var root = document.getElementById('root');
-	ReactDOM.render( <LoginForm />, root );
+	ReactDOM.render( Router, root );
 });
