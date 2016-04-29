3c1d8ca4a9e0339a9db35f1a31cb2076919ffb78
Walid Arghandewal <walid.argh@gmail.com>
2016-04-29 09:23:24 -0700

    Fixed Reviews Model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
 	validates :content, :stars, :user_id, :business_id, presence: true
-	validates :stars, inclusion: {in 1..5}
+	validates :stars, inclusion: {in: 1..5}
 
 	belongs_to :user
 	belongs_to :business
