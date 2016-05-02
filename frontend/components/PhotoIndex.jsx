ddf1c60203b0628702bb747fe6f628ebab3798b7
Walid Arghandewal <walid.argh@gmail.com>
2016-05-02 08:13:57 -0700

    rename formmodal

--- frontend/components/PhotoIndex.jsx
+++ frontend/components/PhotoIndex.jsx
@@ -1,6 +1,20 @@
 var React = require('react');
-
+var BusinessStore = require('../stores/business');
+var ClientActions = require('../actions/ClientActions');
 var PhotoIndex = React.createClass({
+	getInitialState: function () {
+		return ({photos: [], modalIsOpen: false});
+	},
+
+	componentWillMount: function () {
+		this.businessListener.BusinessStore.addListener(this._onChange);
+		ClientActions.fetchPhotos();
+	},
+
+	_onChange: function () {
+		var businesses = BusinessStore.all();
+	},
+
 
 	render: function() {
 		return (
