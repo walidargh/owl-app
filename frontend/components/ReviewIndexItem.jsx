8f2a9e9bf49f1fd7cf4dd5cd6d6414d2c13214ed
Walid Arghandewal <walid.argh@gmail.com>
2016-05-04 15:01:26 -0700

    current user now show up

--- frontend/components/ReviewIndexItem.jsx
+++ frontend/components/ReviewIndexItem.jsx
@@ -1,14 +1,17 @@
 var React = require('react');
 var BusinessStore = require('../stores/business');
+var UserStore = require('../stores/user');
 var ReviewForm = require('../components/ReviewForm');
 
 var ReviewIndexItem = React.createClass({
 
 	render: function() {
+		debugger
 		var review = this.props.review;
+		var username = review.username ? review.username : UserStore.currentUser().user_name;
 		return (
 			<div className="review-index-item">
-				<li className="review-username">{review.username}</li>
+				<li className="review-username">{username}</li>
 				<li className="review-body">{review.body}</li>
 			</div>
 		);
